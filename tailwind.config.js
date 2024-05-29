225304912a2564d7bede0376633f418ad8933f2b
sr2808 <77497236+sr2808@users.noreply.github.com>
2024-05-29 17:05:07 +0530

    first-commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html", "./build/js/*.js"],
+  theme: {
+    extend: {
+      screens: {
+        widescreen: { raw: "(min-aspect-ratio:3/2)" },
+        tallscreen: { raw: "(max-aspect-ratio:13/20)" },
+      },
+      keyframes: {
+        "open-menu": {
+          "0%": { tranform: "scaleY(0)" },
+          "80%": { tranform: "scaleY(1.2)" },
+          "1000%%": { tranform: "scaleY(1)" },
+        },
+      },
+      animation: {
+        "open-menu": "open-menu 0.5s ease-in-out forwords",
+      },
+    },
+  },
+  plugins: [],
+};
